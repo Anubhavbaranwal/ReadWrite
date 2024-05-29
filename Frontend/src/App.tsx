d2af94cfb9e97ab84af374914ee47f8505141764
Anubhavbaranwal <anubhavbaranwal08@gmail.com>
2024-05-29 05:46:52 +0530

    made chnages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,23 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useLocation, useNavigate } from 'react-router-dom'
 import  Signup  from './Pages/Signup'
 import Signin  from './Pages/Signin'
 import Blog  from './Pages/Blog'
 import Blogs from './Pages/Blogs'
 import Publish from './Pages/publish.tsx'
+import { useEffect } from 'react'
 
 function App() {
-
+  const location =useLocation();
+  const navigate=useNavigate();
+  useEffect(() => {
+    document.title = "Blog App"
+    routing();
+  }, [location.pathname])
+  const routing=()=>{
+    if(location.pathname==="/" ){
+      navigate("/blog");
+    }
+  }
   return (
     <>
       <BrowserRouter>
